b34bde20144f4738db36fbb3d3396edd65d392b0
Kingxop <95347735+Kingxop@users.noreply.github.com>
2022-10-20 21:43:16 +0530

    Update and rename Iconfig.py to config.py

--- config.py
+++ config.py
@@ -10,16 +10,16 @@ class Development(Config):
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://lswkiuoh:i_HcH8j46CCnL4vnO-JY8MiEOCazkxUU@lucky.db.elephantsql.com/lswkiuoh" # IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #DB_URI 
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "YOUR VALUE"# IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #session 
+    STRING_SESSION = "1BVtsOMABuxDw6hYRdBk8C7paGsn8-sXTXOfbdohefUd8KUujiu4Wp4fQ1nS9kUoKsE9CnU7YNvnxI-uldrfn42W-i_RCeWXlhL3Phf8giOhiHyvzARrZS7YmPDFvOaAnY-OWlDxbSkjwS1nFGsN9TGyjpIfELwriVPQimTZSRUrfifXtw8VImSCwpZFP-7FrvxhVT94b80na4XvDG1Cr4cXMqo3usOEpS5vO7_va-WUy5ykFZWKC9_iJJX7sti0ZVr63PNkGbNKcAR9yDjFY0rEAzqPOFloBof2X0oM0n91Xv_J1vVxUU7qyO0QaTg9wQ46vk7pRSa18ImIEXHkBE4oDKxSJkZU="# IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #session 
     # --------------------------------------------------
 
-    TG_BOT_TOKEN = "Your value"# create a new bot in @botfather and fill the following vales with bot token
+    TG_BOT_TOKEN = "5775277677:AAH5-b6IPH77FYR1ga3ExhACecTZqTWyGJo"# create a new bot in @botfather and fill the following vales with bot token
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
     PRIVATE_GROUP_BOT_API_ID = -1001502941230
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
-    VC_SESSION = "your assistant id session"
+    VC_SESSION = "1BVtsOMABuxDw6hYRdBk8C7paGsn8-sXTXOfbdohefUd8KUujiu4Wp4fQ1nS9kUoKsE9CnU7YNvnxI-uldrfn42W-i_RCeWXlhL3Phf8giOhiHyvzARrZS7YmPDFvOaAnY-OWlDxbSkjwS1nFGsN9TGyjpIfELwriVPQimTZSRUrfifXtw8VImSCwpZFP-7FrvxhVT94b80na4XvDG1Cr4cXMqo3usOEpS5vO7_va-WUy5ykFZWKC9_iJJX7sti0ZVr63PNkGbNKcAR9yDjFY0rEAzqPOFloBof2X0oM0n91Xv_J1vVxUU7qyO0QaTg9wQ46vk7pRSa18ImIEXHkBE4oDKxSJkZU="
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
