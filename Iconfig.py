180080a7c3dc4320e5eea0cdab8f29695739c901
Kingxop <95347735+Kingxop@users.noreply.github.com>
2022-10-20 20:57:35 +0530

    Update Iconfig.py

--- Iconfig.py
+++ Iconfig.py
@@ -3,19 +3,19 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 4524654
-    API_HASH = "4cfbcd31ec888cc3f0709d8d20607ac8"
+    APP_ID = 18278042
+    API_HASH = "68302885670e8608bf3bdf9bd7c6aaa1"
     # the name to display in your alive message
-    ALIVE_NAME = "Շђคภ๏ร"
+    ALIVE_NAME = "𝗞𝗜𝗡𝗚"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "YOUR VALUE" # IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #DB_URI 
+    DB_URI = "postgres://lswkiuoh:i_HcH8j46CCnL4vnO-JY8MiEOCazkxUU@lucky.db.elephantsql.com/lswkiuoh" # IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #DB_URI 
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
     STRING_SESSION = "YOUR VALUE"# IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #session 
     # --------------------------------------------------
 
     TG_BOT_TOKEN = "Your value"# create a new bot in @botfather and fill the following vales with bot token
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -1001638681164
+    PRIVATE_GROUP_BOT_API_ID = -1001502941230
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
